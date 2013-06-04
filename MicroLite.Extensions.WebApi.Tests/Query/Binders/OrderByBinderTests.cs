1635e19da684a8cfccd8e0c74ee21ac01cded029
Trevor Pilley <trev@trevorpilley.co.uk>
2013-06-04 19:30:26 +0100

    Added a binder to append the order by clause

---
+++ MicroLite.Extensions.WebApi.Tests/Query/Binders/OrderByBinderTests.cs
@@ -0,0 +1,64 @@
+﻿namespace MicroLite.Extensions.WebApi.Tests.Query.Binders
+{
+    using System;
+    using System.Net.Http;
+    using MicroLite.Extensions.WebApi.Query;
+    using MicroLite.Extensions.WebApi.Query.Binders;
+    using MicroLite.Query;
+    using Xunit;
+
+    public class OrderByBinderTests
+    {
+        private enum CustomerStatus
+        {
+            Pending = 0,
+            Active = 1
+        }
+
+        public class WhenCallingBindOrderBy
+        {
+            private readonly SqlQuery sqlQuery;
+
+            public WhenCallingBindOrderBy()
+            {
+                this.sqlQuery = OrderByBinder.BindOrderBy<Customer>(
+                    SqlBuilder.Select("*").From(typeof(Customer)),
+                    new OrderByQueryOption("$orderby=Status desc,Name")).ToSqlQuery();
+            }
+
+            [Fact]
+            public void TheColumnNamesForTheSpecifiedPropertiesShouldBeSetInTheOrderByClause()
+            {
+                var expected = SqlBuilder
+                    .Select("*")
+                    .From(typeof(Customer))
+                    .OrderByDescending("CustomerStatusId")
+                    .OrderByAscending("Name")
+                    .ToSqlQuery();
+
+                Assert.Equal(expected, this.sqlQuery);
+            }
+        }
+
+        private class Customer
+        {
+            public int Id
+            {
+                get;
+                set;
+            }
+
+            public string Name
+            {
+                get;
+                set;
+            }
+
+            public CustomerStatus Status
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
---
+++ MicroLite.Extensions.WebApi/Query/Binders/OrderByBinder.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="OrderByBinder.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Extensions.WebApi.Query.Binders
+{
+    using System.Linq;
+    using MicroLite.Mapping;
+    using MicroLite.Query;
+
+    internal static class OrderByBinder
+    {
+        internal static IOrderBy BindOrderBy<T>(IOrderBy orderBySqlBuilder, OrderByQueryOption orderByQuery)
+        {
+            var objectInfo = ObjectInfo.For(typeof(T));
+
+            foreach (var orderByProperty in orderByQuery.Properties)
+            {
+                var columnName = objectInfo.TableInfo.Columns.Single(c => c.PropertyInfo.Name == orderByProperty.Name).ColumnName;
+
+                if (orderByProperty.Direction == OrderByDirection.Ascending)
+                {
+                    orderBySqlBuilder.OrderByAscending(columnName);
+                }
+                else
+                {
+                    orderBySqlBuilder.OrderByDescending(columnName);
+                }
+            }
+
+            return orderBySqlBuilder;
+        }
+    }
+}
