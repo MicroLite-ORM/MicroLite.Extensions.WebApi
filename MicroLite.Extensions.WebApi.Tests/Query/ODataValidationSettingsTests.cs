f1728c71b62de4242dabb910bdff3494e89670b9
Trevor Pilley <trev@trevorpilley.co.uk>
2013-06-04 20:05:36 +0100

    Started work on ODataValidationSettings

---
+++ MicroLite.Extensions.WebApi.Tests/Query/ODataValidationSettingsTests.cs
@@ -0,0 +1,78 @@
+﻿namespace MicroLite.Extensions.WebApi.Tests.Query
+{
+    using System.Net.Http;
+    using MicroLite.Extensions.WebApi.Query;
+    using MicroLite.Extensions.WebApi.Query.Validation;
+    using Xunit;
+
+    public class ODataValidationSettingsTests
+    {
+        public class WhenValidatingAndAMaxTopIsSetButTheQueryOptionDoesNotSpecifyATopValue
+        {
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(
+                new HttpRequestMessage(HttpMethod.Get, "http://localhost/api"));
+
+            private readonly ODataValidationSettings validationSettings = new ODataValidationSettings
+            {
+                MaxTop = 100
+            };
+
+            [Fact]
+            public void NoExceptionIsThrown()
+            {
+                Assert.DoesNotThrow(() => this.validationSettings.Validate(this.queryOptions));
+            }
+        }
+
+        public class WhenValidatingAndAMaxTopIsSetWhichTheQueryOptionDoesNotExceed
+        {
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(
+                new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$top=25"));
+
+            private readonly ODataValidationSettings validationSettings = new ODataValidationSettings
+            {
+                MaxTop = 100
+            };
+
+            [Fact]
+            public void NoExceptionIsThrown()
+            {
+                Assert.DoesNotThrow(() => this.validationSettings.Validate(this.queryOptions));
+            }
+        }
+
+        public class WhenValidatingAndAMaxTopIsSetWhichTheQueryOptionExceeds
+        {
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(
+                new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$top=150"));
+
+            private readonly ODataValidationSettings validationSettings = new ODataValidationSettings
+            {
+                MaxTop = 100
+            };
+
+            [Fact]
+            public void AnExceptionIsThrown()
+            {
+                Assert.Throws<ODataException>(() => this.validationSettings.Validate(this.queryOptions));
+            }
+        }
+
+        public class WhenValidatingAndNoMaxTopIsSet
+        {
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(
+                new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$top=150"));
+
+            private readonly ODataValidationSettings validationSettings = new ODataValidationSettings
+            {
+                MaxTop = null
+            };
+
+            [Fact]
+            public void NoExceptionIsThrown()
+            {
+                Assert.DoesNotThrow(() => this.validationSettings.Validate(this.queryOptions));
+            }
+        }
+    }
+}
---
+++ MicroLite.Extensions.WebApi/Query/ODataValidationSettings.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ODataValidationSettings.cs" company="MicroLite">
+// Copyright 2012 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Extensions.WebApi.Query.Validation
+{
+    /// <summary>
+    /// A class which validates the values in <see cref="ODataQueryOptions"/>.
+    /// </summary>
+    public sealed class ODataValidationSettings
+    {
+        /// <summary>
+        /// Gets or sets the max value allowed in the $top query option.
+        /// </summary>
+        public int? MaxTop
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Validates the specified query options.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        public void Validate(ODataQueryOptions queryOptions)
+        {
+            if (this.MaxTop.HasValue && queryOptions.Top != null)
+            {
+                if (queryOptions.Top.Value > this.MaxTop.Value)
+                {
+                    throw new ODataException();
+                }
+            }
+        }
+    }
+}
