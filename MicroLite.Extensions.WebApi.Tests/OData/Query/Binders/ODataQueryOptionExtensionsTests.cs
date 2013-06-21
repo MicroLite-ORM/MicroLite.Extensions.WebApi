ef869a2eff8bda969b8509ebd764cbe8ed9aed09
Trevor Pilley <trev@trevorpilley.co.uk>
2013-06-21 19:27:18 +0100

    Added test for ODataQueryOptionExtensions

---
+++ MicroLite.Extensions.WebApi.Tests/OData/Query/Binders/ODataQueryOptionExtensionsTests.cs
@@ -0,0 +1,53 @@
+﻿namespace MicroLite.Extensions.WebApi.Tests.OData.Query.Binders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MicroLite.Extensions.WebApi.OData.Query;
+    using MicroLite.Extensions.WebApi.OData.Query.Binders;
+    using MicroLite.Query;
+    using Xunit;
+
+    public class ODataQueryOptionExtensionsTests
+    {
+        [Fact]
+        public void CreateSqlQueryBindsSelectThenAddsFilterAndOrderBy()
+        {
+            var httpRequestMessage = new HttpRequestMessage(
+                HttpMethod.Get,
+                "http://localhost/api?$filter=Forename eq 'John'&$orderby=Surname");
+
+            var option = new ODataQueryOptions(httpRequestMessage);
+
+            var sqlQuery = option.CreateSqlQuery<Customer>();
+
+            var expected = SqlBuilder.Select("*").From(typeof(Customer)).Where("(Forename = @p0)", "John").OrderByAscending("Surname").ToSqlQuery();
+
+            Assert.Equal(expected, sqlQuery);
+        }
+
+        public class Customer
+        {
+            public string Forename
+            {
+                get;
+                set;
+            }
+
+            public int Id
+            {
+                get;
+                set;
+            }
+
+            public string Surname
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
