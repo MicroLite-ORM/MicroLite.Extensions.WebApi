992af2128c9dd4f7bc749536235ba618ef0f62de
Trevor Pilley <trev@trevorpilley.co.uk>
2013-06-23 15:18:32 +0100

    Added tests for the validation filters

---
+++ MicroLite.Extensions.WebApi.Tests/Filters/ValidateModelNotNullAttributeTests.cs
@@ -0,0 +1,48 @@
+﻿namespace MicroLite.Extensions.WebApi.Tests.Filters
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Routing;
+    using MicroLite.Extensions.WebApi.Filters;
+    using Moq;
+    using Xunit;
+
+    public class ValidateModelNotNullAttributeTests
+    {
+        public class WhenCallingOnActionExecutingAndTheActionArgumentsContainNull
+        {
+            private readonly ValidateModelNotNullAttribute attribute = new ValidateModelNotNullAttribute();
+
+            [Fact]
+            public void TheResponseShouldBeSetToBadRequest()
+            {
+                var controllerContext = new HttpControllerContext(new HttpConfiguration(), new Mock<IHttpRouteData>().Object, new HttpRequestMessage());
+                var actionContext = new HttpActionContext(controllerContext, new Mock<HttpActionDescriptor>().Object);
+                actionContext.ActionArguments.Add("model", null);
+
+                attribute.OnActionExecuting(actionContext);
+
+                Assert.Equal(HttpStatusCode.BadRequest, actionContext.Response.StatusCode);
+            }
+        }
+
+        public class WhenCallingOnActionExecutingAndTheActionArgumentsDoesNotContainNull
+        {
+            private readonly ValidateModelNotNullAttribute attribute = new ValidateModelNotNullAttribute();
+
+            [Fact]
+            public void TheResponseShoulNotBeSet()
+            {
+                var controllerContext = new HttpControllerContext(new HttpConfiguration(), new Mock<IHttpRouteData>().Object, new HttpRequestMessage());
+                var actionContext = new HttpActionContext(controllerContext, new Mock<HttpActionDescriptor>().Object);
+                actionContext.ActionArguments.Add("model", new object());
+
+                attribute.OnActionExecuting(actionContext);
+
+                Assert.Null(actionContext.Response);
+            }
+        }
+    }
+}
---
+++ MicroLite.Extensions.WebApi.Tests/Filters/ValidateModelStateAttributeTests.cs
@@ -0,0 +1,48 @@
+﻿namespace MicroLite.Extensions.WebApi.Tests.Filters
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Routing;
+    using MicroLite.Extensions.WebApi.Filters;
+    using Moq;
+    using Xunit;
+
+    public class ValidateModelStateAttributeTests
+    {
+        public class WhenCallingOnActionExecutingAndTheActionArgumentsContainNull
+        {
+            private readonly ValidateModelStateAttribute attribute = new ValidateModelStateAttribute();
+
+            [Fact]
+            public void TheResponseShouldBeSetToBadRequest()
+            {
+                var controllerContext = new HttpControllerContext(new HttpConfiguration(), new Mock<IHttpRouteData>().Object, new HttpRequestMessage());
+                var actionContext = new HttpActionContext(controllerContext, new Mock<HttpActionDescriptor>().Object);
+                actionContext.ModelState.AddModelError("Foo", "Error");
+
+                attribute.OnActionExecuting(actionContext);
+
+                Assert.Equal(HttpStatusCode.BadRequest, actionContext.Response.StatusCode);
+            }
+        }
+
+        public class WhenCallingOnActionExecutingAndTheActionArgumentsDoesNotContainNull
+        {
+            private readonly ValidateModelStateAttribute attribute = new ValidateModelStateAttribute();
+
+            [Fact]
+            public void TheResponseShoulNotBeSet()
+            {
+                var controllerContext = new HttpControllerContext(new HttpConfiguration(), new Mock<IHttpRouteData>().Object, new HttpRequestMessage());
+                var actionContext = new HttpActionContext(controllerContext, new Mock<HttpActionDescriptor>().Object);
+                actionContext.ModelState.Clear();
+
+                attribute.OnActionExecuting(actionContext);
+
+                Assert.Null(actionContext.Response);
+            }
+        }
+    }
+}
