9f24df7e0b5af7bf2315abf2e61adffef4aa3bbd
Trevor Pilley <trev@trevorpilley.co.uk>
2013-07-12 22:57:13 +0100

    Added the ODataApiController

---
+++ MicroLite.Extensions.WebApi.Tests/OData/MicroLiteODataApiControllerTests.cs
@@ -0,0 +1,200 @@
+﻿namespace MicroLite.Extensions.WebApi.Tests.OData
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Web.Http.Hosting;
+    using MicroLite.Extensions.WebApi.OData;
+    using Moq;
+    using Net.Http.WebApi.OData;
+    using Net.Http.WebApi.OData.Query;
+    using Xunit;
+
+    public class MicroLiteODataApiControllerTests
+    {
+        [Fact]
+        public void WhenCallingGetEntityResponseTheODataQueryOptionsAreValidated()
+        {
+            var queryOptions = new ODataQueryOptions(new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$skip=-1"));
+
+            var controller = new CustomerController();
+
+            var exception = Assert.Throws<ODataException>(() => controller.Get(queryOptions));
+            Assert.Contains("$skip", exception.Message);
+        }
+
+        public class WhenAValidSkipValueIsSpecified
+        {
+            private readonly CustomerController controller = new CustomerController();
+            private readonly Mock<ISession> mockSession = new Mock<ISession>();
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$skip=15"));
+
+            public WhenAValidSkipValueIsSpecified()
+            {
+                this.mockSession.Setup(x => x.Paged<dynamic>(It.IsAny<SqlQuery>(), It.IsAny<PagingOptions>())).Returns(new PagedResult<dynamic>(0, new object[0], 50, 0));
+
+                this.controller.Request = this.queryOptions.Request;
+                this.controller.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+                this.controller.Session = this.mockSession.Object;
+
+                this.controller.Get(this.queryOptions);
+            }
+
+            [Fact]
+            public void ItIsUsedInThePagedQuery()
+            {
+                this.mockSession.Verify(x => x.Paged<dynamic>(It.IsAny<SqlQuery>(), PagingOptions.SkipTake(this.queryOptions.Skip.Value, 50)));
+            }
+        }
+
+        public class WhenAValidTopValueIsSpecified
+        {
+            private readonly CustomerController controller = new CustomerController();
+            private readonly Mock<ISession> mockSession = new Mock<ISession>();
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$top=15"));
+
+            public WhenAValidTopValueIsSpecified()
+            {
+                this.mockSession.Setup(x => x.Paged<dynamic>(It.IsAny<SqlQuery>(), It.IsAny<PagingOptions>())).Returns(new PagedResult<dynamic>(0, new object[0], 15, 0));
+
+                this.controller.Request = this.queryOptions.Request;
+                this.controller.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+                this.controller.Session = this.mockSession.Object;
+
+                this.controller.Get(this.queryOptions);
+            }
+
+            [Fact]
+            public void ItIsUsedInThePagedQuery()
+            {
+                this.mockSession.Verify(x => x.Paged<dynamic>(It.IsAny<SqlQuery>(), PagingOptions.SkipTake(0, this.queryOptions.Top.Value)));
+            }
+        }
+
+        public class WhenFormatQueryOptionIsSpecified
+        {
+            private readonly CustomerController controller = new CustomerController();
+            private readonly Mock<ISession> mockSession = new Mock<ISession>();
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$format=application/xml"));
+            private readonly HttpResponseMessage response;
+
+            public WhenFormatQueryOptionIsSpecified()
+            {
+                this.mockSession.Setup(x => x.Paged<dynamic>(It.IsAny<SqlQuery>(), It.IsAny<PagingOptions>())).Returns(new PagedResult<dynamic>(0, new object[0], 50, 0));
+
+                this.controller.Request = this.queryOptions.Request;
+                this.controller.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+                this.controller.Session = this.mockSession.Object;
+
+                this.response = this.controller.Get(this.queryOptions);
+            }
+
+            [Fact]
+            public void TheHttpResponseMessageShouldHaveHttpStatusCodeOK()
+            {
+                Assert.Equal(HttpStatusCode.OK, this.response.StatusCode);
+            }
+
+            [Fact]
+            public void TheResponseContentTypeHeaderIsSet()
+            {
+                Assert.Equal(this.queryOptions.Format.MediaTypeHeaderValue, response.Content.Headers.ContentType);
+            }
+        }
+
+        public class WhenInlineCountAllPagesIsSpecified
+        {
+            private readonly CustomerController controller = new CustomerController();
+            private readonly Mock<ISession> mockSession = new Mock<ISession>();
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(new HttpRequestMessage(HttpMethod.Get, "http://localhost/api?$inlinecount=allpages"));
+            private readonly HttpResponseMessage response;
+
+            public WhenInlineCountAllPagesIsSpecified()
+            {
+                this.mockSession.Setup(x => x.Paged<dynamic>(It.IsAny<SqlQuery>(), It.IsAny<PagingOptions>())).Returns(new PagedResult<dynamic>(0, new object[0], 50, 0));
+
+                this.controller.Request = this.queryOptions.Request;
+                this.controller.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+                this.controller.Session = this.mockSession.Object;
+
+                this.response = this.controller.Get(this.queryOptions);
+            }
+
+            [Fact]
+            public void TheHttpResponseMessageShouldHaveHttpStatusCodeOK()
+            {
+                Assert.Equal(HttpStatusCode.OK, this.response.StatusCode);
+            }
+
+            [Fact]
+            public void TheResponseIsAnInlineCountCollection()
+            {
+                Assert.IsType<InlineCountCollection<dynamic>>(((ObjectContent)this.response.Content).Value);
+            }
+        }
+
+        public class WhenInlineCountIsNotSpecified
+        {
+            private readonly CustomerController controller = new CustomerController();
+            private readonly Mock<ISession> mockSession = new Mock<ISession>();
+            private readonly ODataQueryOptions queryOptions = new ODataQueryOptions(new HttpRequestMessage(HttpMethod.Get, "http://localhost/api"));
+            private readonly HttpResponseMessage response;
+
+            public WhenInlineCountIsNotSpecified()
+            {
+                this.mockSession.Setup(x => x.Paged<dynamic>(It.IsAny<SqlQuery>(), It.IsAny<PagingOptions>())).Returns(new PagedResult<dynamic>(0, new List<object>(), 50, 0));
+
+                this.controller.Request = this.queryOptions.Request;
+                this.controller.Request.Properties.Add(HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration());
+                this.controller.Session = this.mockSession.Object;
+
+                this.response = this.controller.Get(this.queryOptions);
+            }
+
+            [Fact]
+            public void TheHttpResponseMessageShouldHaveHttpStatusCodeOK()
+            {
+                Assert.Equal(HttpStatusCode.OK, this.response.StatusCode);
+            }
+
+            [Fact]
+            public void TheResponseIsAnList()
+            {
+                Assert.IsType<List<dynamic>>(((ObjectContent)this.response.Content).Value);
+            }
+        }
+
+        private class Customer
+        {
+            public int Id
+            {
+                get;
+                set;
+            }
+
+            public string Name
+            {
+                get;
+                set;
+            }
+        }
+
+        private class CustomerController : MicroLiteODataApiController<Customer, int>
+        {
+            public CustomerController()
+            {
+                this.GetEntityResourceUri = (int id) =>
+                {
+                    return new Uri("http://localhost/api/Customers/" + id.ToString());
+                };
+            }
+
+            public HttpResponseMessage Get(ODataQueryOptions queryOptions)
+            {
+                return this.GetEntityResponse(queryOptions);
+            }
+        }
+    }
+}
---
+++ MicroLite.Extensions.WebApi/OData/MicroLiteODataApiController{T}.cs
@@ -0,0 +1,109 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MicroLiteODataApiController{T}.cs" company="MicroLite">
+// Copyright 2012-2013 Trevor Pilley
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// </copyright>
+// -----------------------------------------------------------------------
+namespace MicroLite.Extensions.WebApi.OData
+{
+    using System.Net;
+    using System.Net.Http;
+    using MicroLite.Extensions.WebApi.OData.Binders;
+    using Net.Http.WebApi.OData.Query;
+    using Net.Http.WebApi.OData.Query.Validation;
+
+    /// <summary>
+    /// A controller which adds support for OData queries to the standard <see cref="MicroLiteApiController&lt;TEntity, TId&gt;"/>.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
+    /// <typeparam name="TId">The type of the id.</typeparam>
+    public abstract class MicroLiteODataApiController<TEntity, TId> : MicroLiteApiController<TEntity, TId>
+        where TEntity : class, new()
+    {
+        private readonly ODataValidationSettings validationSettings;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MicroLiteODataApiController{TEntity, TId}"/> class.
+        /// </summary>
+        protected MicroLiteODataApiController()
+        {
+            this.validationSettings = new ODataValidationSettings
+            {
+                AllowedQueryOptions = AllowedQueryOptions.Filter
+                    | AllowedQueryOptions.Format
+                    | AllowedQueryOptions.InlineCount
+                    | AllowedQueryOptions.OrderBy
+                    | AllowedQueryOptions.Select
+                    | AllowedQueryOptions.Skip
+                    | AllowedQueryOptions.Top,
+                MaxTop = 50
+            };
+        }
+
+        /// <summary>
+        /// Gets or the Validation Settings for the OData query.
+        /// </summary>
+        protected ODataValidationSettings ValidationSettings
+        {
+            get
+            {
+                return this.validationSettings;
+            }
+        }
+
+        /// <summary>
+        /// Creates the SQL query to use based upon the values in the specified ODataQueryOptions.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>The SqlQuery to execute.</returns>
+        protected virtual SqlQuery CreateSqlQuery(ODataQueryOptions queryOptions)
+        {
+            var sqlQuery = queryOptions.CreateSqlQuery<TEntity>();
+
+            return sqlQuery;
+        }
+
+        /// <summary>
+        /// Gets the entity results based upon the OData query.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>The an <see cref="HttpResponseMessage"/> with the execution result.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "The whole point to this method is that it returns the object!")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Get", Justification = "In WebApi the convention is to use the HTTP Verb as the method name")]
+        protected virtual HttpResponseMessage GetEntityResponse(ODataQueryOptions queryOptions)
+        {
+            queryOptions.Validate(this.validationSettings);
+
+            var sqlQuery = this.CreateSqlQuery(queryOptions);
+
+            var skip = queryOptions.Skip != null ? queryOptions.Skip.Value : 0;
+            var top = queryOptions.Top != null ? queryOptions.Top.Value : this.validationSettings.MaxTop;
+
+            var paged = this.Session.Paged<dynamic>(sqlQuery, PagingOptions.SkipTake(skip, top));
+
+            HttpResponseMessage response;
+
+            if (queryOptions.InlineCount != null && queryOptions.InlineCount.InlineCount == InlineCount.AllPages)
+            {
+                response = this.Request.CreateResponse(HttpStatusCode.OK, new InlineCountCollection<dynamic>(paged.Results, paged.TotalResults));
+            }
+            else
+            {
+                response = this.Request.CreateResponse(HttpStatusCode.OK, paged.Results);
+            }
+
+            if (queryOptions.Format != null)
+            {
+                response.Content.Headers.ContentType = queryOptions.Format.MediaTypeHeaderValue;
+            }
+
+            return response;
+        }
+    }
+}
