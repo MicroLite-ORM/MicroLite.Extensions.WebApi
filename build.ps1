28095dd5cb4a1863deeb256ef48ec1fe13c2340f
Trevor Pilley <trev@trevorpilley.co.uk>
2013-09-05 21:33:05 +0100

    Aligned build script with MicroLite build script

--- build.ps1
+++ build.ps1
@@ -10,13 +10,20 @@ $buildDir = "$scriptPath\build"
 $nuGetExe = "$scriptPath\.nuget\NuGet.exe"
 $nuSpec = "$scriptPath\$projectName.nuspec"
 $nuGetPackage = "$buildDir\$projectName.$version.nupkg"
+$date = Get-Date
+$gitDir = $scriptPath + "\.git"
+$commit = git --git-dir $gitDir log -1 --pretty=format:%h
 
 function UpdateAssemblyInfoFiles ([string] $buildVersion)
 {
 	$assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
 	$fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+	$infoVersionPattern = 'AssemblyInformationalVersion\("[0-9]+(\.([0-9]+|\*)){1,3}(.*)"\)'
+	$copyrightPattern = 'AssemblyCopyright\(".+"\)'
 	$assemblyVersion = 'AssemblyVersion("' + $buildVersion.SubString(0, 3) + '.0.0")';
 	$fileVersion = 'AssemblyFileVersion("' + $buildVersion.SubString(0, 5) + '.0")';
+	$infoVersion = 'AssemblyInformationalVersion("' + $buildVersion + ' (' + $commit + ')")';
+	$copyright = 'AssemblyCopyright("Copyright 2012-' + $date.Year + ' MicroLite Project Contributors all rights reserved.")';
 	
 	Get-ChildItem $scriptPath -r -filter AssemblyInfo.cs | ForEach-Object {
 		$filename = $_.Directory.ToString() + '\' + $_.Name
@@ -24,7 +31,9 @@ function UpdateAssemblyInfoFiles ([string] $buildVersion)
 			
 		(Get-Content $filename) | ForEach-Object {
 			% {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
-			% {$_ -replace $fileVersionPattern, $fileVersion }
+			% {$_ -replace $fileVersionPattern, $fileVersion } |
+			% {$_ -replace $infoVersionPattern, $infoVersion } |
+			% {$_ -replace $copyrightPattern, $copyright }
 		} | Set-Content $filename -Encoding UTF8
 	}
 }
