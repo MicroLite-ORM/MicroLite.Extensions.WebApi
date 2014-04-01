1e5cb2f27a8802beaa9993ad1a53bdb9caf815b7
Trevor Pilley <trev@trevorpilley.co.uk>
2014-04-01 19:56:43 +0100

    Added tests for WebApiConfigurationSettings

---
+++ MicroLite.Extensions.WebApi.Tests/WebApiConfigurationSettingsTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MicroLite.Extensions.WebApi.Tests
+{
+    using MicroLite.Configuration;
+    using Xunit;
+
+    public class WebApiConfigurationSettingsTests
+    {
+        [Fact]
+        public void PropertiesAreSetToTrueByDefault()
+        {
+            var settings = new WebApiConfigurationSettings();
+
+            Assert.True(settings.RegisterGlobalMicroLiteSessionAttribute);
+            Assert.True(settings.RegisterGlobalValidateModelNotNullAttribute);
+            Assert.True(settings.RegisterGlobalValidateModelStateAttribute);
+        }
+    }
+}
