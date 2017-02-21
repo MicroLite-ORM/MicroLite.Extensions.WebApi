cc30b0454ec57cbb44563114a0fb3ca5755fa741
Trevor Pilley <trev@trevorpilley.co.uk>
2017-02-21 09:45:53 +0000

    Built MicroLite.Extensions.WebApi 6.5.1

--- MicroLite.Extensions.WebApi.Tests/Properties/AssemblyInfo.cs
+++ MicroLite.Extensions.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("6.5.0.0")]
 [assembly: AssemblyVersion("6.5.0.0")]
-[assembly: AssemblyFileVersion("6.5.0.0")]
+[assembly: AssemblyFileVersion("6.5.1.0")]
