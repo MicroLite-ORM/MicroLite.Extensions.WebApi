3d76610735834c8f09bd6c1d4ad5a6cc67c93191
Trevor Pilley <trev@trevorpilley.co.uk>
2017-06-26 17:02:43 +0100

    Updated to xunit 2.0
    Disabled parallel tests for now as some were failing

--- MicroLite.Extensions.WebApi.Tests/Properties/AssemblyInfo.cs
+++ MicroLite.Extensions.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("6.5.0.0")]
 [assembly: AssemblyVersion("6.5.0.0")]
 [assembly: AssemblyFileVersion("6.5.1.0")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
